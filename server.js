7bdfb52a1cce54ce24c8e46a2de89b8373858518
Mahdi H Mohamed <mr.mahdimohamediii@gmail.com>
2019-04-28 17:21:35 -0500

    increased scope

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/login', function(req, res) {
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
+      scope: 'user-read-private user-read-email user-read-recently-played user-top-read user-follow-read user-follow-modify playlist-read-private playlist-read-collaborative playlist-modify-public ',
       redirect_uri
     }))
 })
